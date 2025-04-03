c793badb595b8c776cee3a822da099ba4ad5ff67
Aman Roniwal <amanroniwal@Amans-MacBook-Air-2.local>
2025-04-03 10:09:06 +0530

    schedule

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -7,6 +7,7 @@ import { api } from "../../../../convex/_generated/api";
 import { useQuery } from "convex/react";
 import { useRouter } from "next/navigation";
 import MeetingModal from "@/components/MeetingModal";
+import LoaderUI from "@/components/LoaderUI";
 
 export default function Home(){
   const router = useRouter();
@@ -35,7 +36,7 @@ export default function Home(){
 
   };
 
-  if(isLoading) return <p>loading...</p>
+  if(isLoading) return <LoaderUI />
   
 
   return <div className="container max-w-7xl mx-auto p-6">
